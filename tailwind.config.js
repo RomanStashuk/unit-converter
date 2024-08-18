4696464f0067a4622de46cd1b39281759606cf33
RomanStashuk <roman.stashuk.01@gmail.com>
2024-08-19 01:31:46 +0300

    install flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const flowbite = require('flowbite-react/tailwind');
+
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
+    flowbite.content(),
+  ],
   relative: true,
   darkMode: 'selector',
   theme: {},
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require('@tailwindcss/forms'), flowbite.plugin()],
 };
