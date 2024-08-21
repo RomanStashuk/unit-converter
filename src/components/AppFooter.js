4c1e3e823768a4851e6b07c52a27719752d99366
RomanStashuk <roman.stashuk.01@gmail.com>
2024-08-21 23:20:30 +0300

    update styles

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -4,14 +4,16 @@ const AppFooter = () => {
   return (
     <Footer
       container
-      className='rounded-none bg-gradient-to-r from-violet-400 to-purple-300 shadow shadow-slate-200 dark:from-slate-950 dark:to-slate-700'
+      className='rounded-none bg-gradient-to-r from-violet-400 to-purple-300 shadow shadow-slate-200 dark:from-slate-900 dark:to-slate-800'
     >
-      <Footer.Copyright
-        className='text-slate-700 dark:text-slate-200'
-        href='https://github.com/RomanStashuk'
-        by='Roman Stashuk'
-        year={2024}
-      />
+      <div className='w-full text-center'>
+        <Footer.Copyright
+          className='text-slate-800 dark:text-slate-50'
+          href='https://github.com/RomanStashuk'
+          by='Roman Stashuk'
+          year={2024}
+        />
+      </div>
     </Footer>
   );
 };
--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -2,13 +2,13 @@ import { DarkThemeToggle } from 'flowbite-react';
 
 const AppHeader = () => {
   return (
-    <header className='bg-gradient-to-r from-violet-400 to-purple-300 shadow shadow-slate-200 dark:from-slate-700 dark:to-slate-950'>
+    <header className='bg-gradient-to-r from-violet-400 to-purple-300 shadow shadow-slate-200 dark:from-slate-900 dark:to-slate-800'>
       <div className='container mx-auto flex items-center justify-between px-3 py-3'>
-        <h1 className='text-xl font-semibold text-slate-700 dark:text-slate-200'>
+        <h1 className='text-xl font-semibold text-slate-800 dark:text-slate-50'>
           Units converter App
         </h1>
         <p>
-          <DarkThemeToggle className='text-slate-700 dark:text-slate-200' />
+          <DarkThemeToggle className='text-slate-800 dark:text-slate-50' />
         </p>
       </div>
     </header>
